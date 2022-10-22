fde569ddb8f66b68179134c8be39483dc13e259e
Anirudha Gupta <me@anirudhagupta.com>
2022-10-22 03:48:58 +0000

    saving progress

--- IsPalindromecpp.cpp
+++ IsPalindromecpp.cpp
@@ -10,12 +10,14 @@ int main()
 	cout << result;
 }
 
-bool isPalindrome(int x) {
+bool isPalindrome(int x)
+{
 	if (x < 0)
 		return false;
 	int reverse = 0;
 	int palindrom = x;
-	while (palindrom != 0) {
+	while (palindrom != 0)
+	{
 		int remainder = palindrom % 10;
 		reverse = reverse * 10 + remainder;
 		palindrom = palindrom / 10;
