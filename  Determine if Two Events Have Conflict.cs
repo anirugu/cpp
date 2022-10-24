264e402b30fb494fdbead01aabf1fc6338852d72
Anirudha Gupta <me@anirudhagupta.com>
2022-10-24 13:40:57 +0000

    add non working code

---
+++  Determine if Two Events Have Conflict.cs
@@ -0,0 +1,18 @@
+class Test
+{
+    public static void Main()
+    {
+        Solution solution = new Solution();
+        decimal resullt = decimal.Parse("01:15");
+        //solution.HaveConflict("");
+    }
+}
+
+class Solution
+{
+
+    public bool HaveConflict(string[] event1, string[] event2)
+    {
+        
+    }
+}
